83c9170e8a3c2a037d8f877061d41703e4ee4c06
Aleksander Alekseev <shure348@gmail.com>
2018-03-25 15:24:50 +0300

    ARTICLE

--- build/js/ready.js
+++ build/js/ready.js
@@ -5,3 +5,23 @@ $(function(){
 		$('.main_menu__overlay').toggleClass('active')
 	})
 })
+
+
+$(function(){
+	baguetteBox.run('.baguetteBox');
+})
+
+
+	$(function(){
+		$('.article_slider').slick({
+			slidesToShow: 3,
+			initialSlide: 1,
+			infinite: false,
+
+			prevArrow: '<a class="left carousel-control" href="javascript:;"><i class="icon-angle-left"></i></a>',
+			nextArrow: '<a class="right carousel-control" href="javascript:;"><i class="icon-angle-right"></i></a>'
+
+		});
+
+
+	})
