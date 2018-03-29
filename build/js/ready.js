0328f91d925eb33a2be244c2b4d2e26d0be100c3
Aleksander Alekseev <shure348@gmail.com>
2018-03-29 08:55:47 +0300

    main_menu

--- build/js/ready.js
+++ build/js/ready.js
@@ -12,23 +12,26 @@ $(function(){
 })
 
 
-	$(function(){
-		$('.article_slider').slick({
-			slidesToShow: 3,
-			initialSlide: 1,
-			infinite: false,
+$(function(){
+	$('.article_slider').slick({
+		slidesToShow: 3,
+		initialSlide: 1,
+		infinite: false,
 
-			prevArrow: '<a class="left carousel-control" href="javascript:;"><i class="icon-angle-left"></i></a>',
-			nextArrow: '<a class="right carousel-control" href="javascript:;"><i class="icon-angle-right"></i></a>',
-			responsive: [
-		    {
-		      breakpoint: 768,
-		      settings: {
-						slidesToShow: 2
-		      }
-		    }
-		  ]
-		});
+		prevArrow: '<a class="left carousel-control" href="javascript:;"><i class="icon-angle-left"></i></a>',
+		nextArrow: '<a class="right carousel-control" href="javascript:;"><i class="icon-angle-right"></i></a>',
+		responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+					slidesToShow: 2
+	      }
+	    }
+	  ]
+	});
+})
 
 
-	})
+$(function(){
+	//$('.main_menu__list').timeoutMenu()
+})
